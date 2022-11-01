9ab497e71b2c2084918d28b084cfdf019561c609
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-11-01 17:34:39 +0900

    [FE] questionPage

--- front/src/App.js
+++ front/src/App.js
@@ -5,18 +5,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./component/MainPage";
 import RedirectPage from "./component/RedirectPage";
 import Header from "./component/Header";
+import SolvePage from "./component/SolvePage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <div>
-        <Header></Header>
-      </div>
+        <div>
+          <Header></Header>
+        </div>
         <Routes>
           <Route exact path="/" element={<LoginPage />}></Route>
           <Route path="/oauth/redirect" element={<RedirectPage />}></Route>
           <Route path="/mainpage" element={<MainPage />}></Route>
+          <Route path="/solve" element={<SolvePage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
