22c4015031d34fe905ba2e6e24d15ae781755a65
이경준 <oct301@naver.com>
2022-11-04 14:07:58 +0900

    [FE] feat: pull conflict fixed

--- front/src/api/user.js
+++ front/src/api/user.js
@@ -1,34 +1,27 @@
 import { instance, createHeaders } from "./index";
 
-// accountrole, accountSuccessCount, usingBadgeId, 
-function getUserProfile(token, success, error){
-  instance.get("/mypage",  { headers: createHeaders(token) }).then(success).catch(error);
-}
-
-function apitest(token, success, error) {
+// accountrole, accountSuccessCount, usingBadgeId,
+function getUserProfile(token, success, error) {
   instance
-<<<<<<< HEAD
-    .get("/question/1", {
-      headers: createHeaders(token),
-    })
-=======
-    .get("/test111", { headers: createHeaders(token) })
->>>>>>> 94e0a109413ef20ede8b88a9b1325cd632321cf3
+    .get("/mypage", { headers: createHeaders(token) })
     .then(success)
     .catch(error);
 }
 
-<<<<<<< HEAD
+function apitest(token, success, error) {
+  instance
+    .get("/question/1", {
+      headers: createHeaders(token),
+    })
+    .then(success)
+    .catch(error);
+}
+
 function apitest2(token, success, error) {
   instance
     .get("/mypage", { headers: createHeaders(token) })
     .then(success)
     .catch(error);
 }
-=======
->>>>>>> 94e0a109413ef20ede8b88a9b1325cd632321cf3
 
-export {
-  getUserProfile,
-  apitest,
-};
+export { getUserProfile, apitest };
