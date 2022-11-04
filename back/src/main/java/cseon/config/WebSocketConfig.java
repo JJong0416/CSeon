b5854e5325f94ec5dbc7bc18e4a811a81f5dc44e
이경준 <oct301@naver.com>
2022-11-04 17:54:23 +0900

    [BE] feat: websocketconfig setting

---
+++ back/src/main/java/cseon/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package cseon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/webSocket")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic","/queue");
+        registry.setApplicationDestinationPrefixes("/");
+    }
+}
