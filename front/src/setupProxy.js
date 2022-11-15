70b602b75755591eb27a6f6cb139c3300f8a175f
이경준 <oct301@naver.com>
2022-11-15 14:32:40 +0900

    [FE] fix: url fixed

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:3000",
+      target: "http://localhost:7070",
       changeOrigin: true,
     })
   );
