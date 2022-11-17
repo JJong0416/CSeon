661c48dc35b1fd73270c21e261be18e9fdb8fa02
박예인 <iasfk@naver.com>
2022-11-17 17:22:08 +0900

    Update IndividualLogsConsumer/src/main/java/com/logs/individuallogsconsumer/service/LogsService.java

--- IndividualLogsConsumer/src/main/java/com/logs/individuallogsconsumer/service/LogsService.java
+++ IndividualLogsConsumer/src/main/java/com/logs/individuallogsconsumer/service/LogsService.java
@@ -10,12 +10,15 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 @RequiredArgsConstructor
 public class LogsService {
     private final TryRepository tryRepository;
     private final SolvedRepository solvedRepository;
+    private final Logger logger = LoggerFactory.getLogger("LogsService.class");
 
     @KafkaListener(topics = "cseon.logs.try", groupId = "history", concurrency = "3")
     public void onMessage(ConsumerRecords<String, String> records, Acknowledgment ack) {
@@ -24,23 +27,23 @@ public class LogsService {
             // 0:accountName 1:timestamp 2:questionId, 3:checkNumber 4:isAnswer
             String[] log = record.value().split(" ");
 
-            System.out.println("1");
+            logger.info("1");
             for(String s : log){
                 System.out.println(s);
             }
 
-            System.out.println("2");
+            logger.info("2");
             // 계정 당 틀린 문제, 맞은 문제 검색
             AccountSolved accountSolved = solvedRepository.findById(log[0])
                     .orElseGet(
                             () -> new AccountSolved(log[0])
                     );
 
-            System.out.println("3");
+            logger.info("3");
             // 틀린 문제, 맞은 문제 체크
             accountSolved.check(Long.parseLong(log[2]), Boolean.parseBoolean(log[4]));
 
-            System.out.println("4");
+            logger.info("4");
             // log 저장
             tryRepository.save(Tries.builder()
                     .accountName(log[0])
@@ -50,7 +53,7 @@ public class LogsService {
                     .isAnswer(Boolean.parseBoolean(log[4]))
                     .build());
 
-            System.out.println("5");
+            logger.info("5");
             // 틀린 문제, 맞은 문제 저장
             solvedRepository.save(accountSolved);
         }
