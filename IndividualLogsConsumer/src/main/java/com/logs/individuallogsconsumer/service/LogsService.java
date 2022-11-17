4ed41e16fc0cca50ba10ba70d94d57b1c40c4651
박예인 <iasfk@naver.com>
2022-11-17 16:54:38 +0900

    Update IndividualLogsConsumer/src/main/java/com/logs/individuallogsconsumer/service/LogsService.java

--- IndividualLogsConsumer/src/main/java/com/logs/individuallogsconsumer/service/LogsService.java
+++ IndividualLogsConsumer/src/main/java/com/logs/individuallogsconsumer/service/LogsService.java
@@ -24,15 +24,23 @@ public class LogsService {
             // 0:accountName 1:timestamp 2:questionId, 3:checkNumber 4:isAnswer
             String[] log = record.value().split(" ");
 
+            System.out.println("1");
+            for(String s : log){
+                System.out.println(s);
+            }
+
+            System.out.println("2");
             // 계정 당 틀린 문제, 맞은 문제 검색
             AccountSolved accountSolved = solvedRepository.findById(log[0])
                     .orElseGet(
                             () -> new AccountSolved(log[0])
                     );
 
+            System.out.println("3");
             // 틀린 문제, 맞은 문제 체크
             accountSolved.check(Long.parseLong(log[2]), Boolean.parseBoolean(log[4]));
 
+            System.out.println("4");
             // log 저장
             tryRepository.save(Tries.builder()
                     .accountName(log[0])
@@ -42,6 +50,7 @@ public class LogsService {
                     .isAnswer(Boolean.parseBoolean(log[4]))
                     .build());
 
+            System.out.println("5");
             // 틀린 문제, 맞은 문제 저장
             solvedRepository.save(accountSolved);
         }
