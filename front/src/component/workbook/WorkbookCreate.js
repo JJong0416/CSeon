f3f854ea1165cbefef6d35fd35fcb72076808ce4
이경준 <oct301@naver.com>
2022-11-20 00:16:35 +0900

    [FE] fix: api name changed

--- front/src/component/workbook/WorkbookCreate.js
+++ front/src/component/workbook/WorkbookCreate.js
@@ -103,7 +103,7 @@ export default function WorkbookCreate() {
         }
       );
     } else {
-      getAllWorkbookList(
+      getAllQuestionList(
         Token,
         (res) => {
           setLeft(res.data);
