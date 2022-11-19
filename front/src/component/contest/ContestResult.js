fa819fa21ad1316a79b05c96c435c0cabb6e2e91
이경준 <oct301@naver.com>
2022-11-19 23:34:37 +0900

    [FE] feat: contestResult 참여 안했을 경우 myRank null처리

--- front/src/component/contest/ContestResult.js
+++ front/src/component/contest/ContestResult.js
@@ -23,6 +23,7 @@ export default function ContestResult() {
       contestId,
       Token,
       (res) => {
+        console.log("getContestResult res.data: ", res.data);
         setWorkbookId(res.data.workbookId);
         setRanking(res.data.contestInfoRes.highRanking);
         setMyRank(res.data.contestInfoRes.contestMyRankingRes);
@@ -66,19 +67,21 @@ export default function ContestResult() {
             index={i}
           ></RankComponent>
         ))}
-        {myRank.isExistMeInLeaderboard === true ? null : (
-          <div>
-            ...
-            <RankComponent
-              key={Math.random()}
-              nickname={accountName}
-              score={myRank.myScore}
-              myrank={myRank.myRank}
-              index={myRank.myRank}
-            ></RankComponent>
-            ...
-          </div>
-        )}
+        {myRank !== null ? (
+          myRank.isExistMeInLeaderboard === true ? null : (
+            <div>
+              ...
+              <RankComponent
+                key={Math.random()}
+                nickname={accountName}
+                score={myRank.myScore}
+                myrank={myRank.myRank}
+                index={myRank.myRank}
+              ></RankComponent>
+              ...
+            </div>
+          )
+        ) : null}
       </div>
     </div>
   );
