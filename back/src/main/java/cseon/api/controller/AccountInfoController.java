665807a482f583b9791eafe38346bd03296ddb9c
박종찬 <whdcks5681@naver.com>
2022-11-19 23:46:06 +0900

    [BE] refactor : Badge 제거

--- back/src/main/java/cseon/api/controller/AccountInfoController.java
+++ back/src/main/java/cseon/api/controller/AccountInfoController.java
@@ -2,17 +2,13 @@ package cseon.api.controller;
 
 import cseon.api.dto.response.AccountDetailsRes;
 import cseon.api.dto.response.AccountTypeRes;
-import cseon.api.dto.response.BadgeRes;
 import cseon.api.service.AccountService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,14 +32,5 @@ public class AccountInfoController {
     @GetMapping("/accountInfo")
     public ResponseEntity<AccountTypeRes> getAccountInfo() {
         return ResponseEntity.ok(accountService.getAccountInfo());
-    }
-
-    /**
-     * 유저가 마이페이지에 들어가서 어떤 벳지가 있는지 확인을 한다.
-     */
-    @GetMapping("/mypage/badge")
-    public ResponseEntity<List<BadgeRes>> getAccountMyBadge() {
-        // account 정보 가져오기
-        return new ResponseEntity<>(accountService.getMyBadge(), HttpStatus.OK);
     }
 }
--- back/src/main/java/cseon/api/service/AccountService.java
+++ back/src/main/java/cseon/api/service/AccountService.java
@@ -2,12 +2,15 @@ package cseon.api.service;
 
 import cseon.api.dto.response.AccountDetailsRes;
 import cseon.api.dto.response.AccountTypeRes;
-import cseon.api.dto.response.BadgeRes;
 import cseon.api.dto.response.WorkbookRes;
-import cseon.api.repository.*;
+import cseon.api.repository.AccountRepository;
+import cseon.api.repository.SolvedRepository;
+import cseon.api.repository.WorkbookRepository;
 import cseon.common.exception.CustomException;
 import cseon.common.exception.ErrorCode;
-import cseon.domain.*;
+import cseon.domain.Account;
+import cseon.domain.SolvedQuestion;
+import cseon.domain.Workbook;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,8 +26,6 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final WorkbookRepository workbookRepository;
-    private final AccountBadgeRepository accountBadgeRepository;
-    private final BadgeRepository badgeRepository;
     private final SolvedRepository solvedRepository;
 
 
@@ -45,9 +46,6 @@ public class AccountService {
                         .build())
                 .collect(Collectors.toList());
 
-        // 3. BadgeName을 가져오기 위해 DB를 찔러준다.
-        var badge = readBadge(account.getUsingBadgeId());
-
         // 4. 맞은 문제 리스트, 틀린 문제 리스트를 가져온다.
         SolvedQuestion lists = solvedRepository.findById(getAccountName())
                 .orElseGet(() -> new SolvedQuestion(getAccountName()));
@@ -56,30 +54,10 @@ public class AccountService {
         // 5. 그 후, 필요로 하는 정보들을 Response에 담아 전송한다
         return AccountDetailsRes.builder()
                 .accountSuccessCount(account.getSuccessCount())
-                .badgeName(badge.getBadgeName())
                 .workbooks(workbookRes)
                 .correct(lists.getCorrectQuestion())
                 .wrong(lists.getWrongQuestion())
                 .build();
-    }
-
-
-    @Transactional(readOnly = true)
-    public List<BadgeRes> getMyBadge() {
-        String accountName = getAccountName();
-
-        var account = hasAccountWithAccountName(accountName);
-
-        List<AccountBadge> accountBadges = accountBadgeRepository.findAccountBadgeByAccount(account);
-
-        return accountBadges.stream()
-                .map(AccountBadge::getBadgeId)
-                .map(this::readBadge)
-                .map(badge -> BadgeRes.builder()
-                        .badgeId(badge.getBadgeId())
-                        .badgeName(badge.getBadgeName())
-                        .badgeExp(badge.getBadgeExp()).build())
-                .collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
@@ -103,11 +81,5 @@ public class AccountService {
                 .orElseThrow(() -> {
                     throw new CustomException(ErrorCode.USER_NOT_FOUND);
                 });
-    }
-
-    private Badge readBadge(Long badgeId) {
-        return badgeRepository.findById(badgeId).orElseThrow(() -> {
-            throw new CustomException(ErrorCode.BADGE_NOT_FOUND);
-        });
     }
 }
