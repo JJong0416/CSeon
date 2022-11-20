98b1e5b478a0649673a434a1bf206785a8af0cd9
이경준 <oct301@naver.com>
2022-11-20 21:47:59 +0900

    [FE] feat: box deleted

--- front/src/component/SideBar.js
+++ front/src/component/SideBar.js
@@ -10,41 +10,41 @@ import { SET_QUESTION_INDEX } from "../redux/QuestionInfo";
 import { AutoSizer, List } from "react-virtualized";
 
 export default function SideBar(props) {
-  function renderRow(p) {
-    const { data, index } = p;
-    const current = index === questionIndex;
+  // function renderRow(p) {
+  //   const { data, index } = p;
+  //   const current = index === questionIndex;
 
-    const Clicklistitem = () => {
-      dispatch(SET_QUESTION_INDEX(index));
-    };
-    console.log("index: ", index);
-    return (
-      <ListItem
-        key={index}
-        component="div"
-        disablePadding
-        style={
-          current
-            ? {
-                borderBottom: "solid #9DCFFF 1px",
-                width: "90%",
-                margin: "auto",
-                backgroundColor: "#9DCFFF",
-              }
-            : {
-                borderBottom: "solid #9DCFFF 1px",
-                width: "90%",
-                margin: "auto",
-                backgroundColor: "white",
-              }
-        }
-      >
-        <ListItemButton onClick={Clicklistitem}>
-          <ListItemText primary={index + 1 + `번문제`} />
-        </ListItemButton>
-      </ListItem>
-    );
-  }
+  //   const Clicklistitem = () => {
+  //     dispatch(SET_QUESTION_INDEX(index));
+  //   };
+  //   console.log("index: ", index);
+  //   return (
+  //     <ListItem
+  //       key={index}
+  //       component="div"
+  //       disablePadding
+  //       style={
+  //         current
+  //           ? {
+  //               borderBottom: "solid #9DCFFF 1px",
+  //               width: "90%",
+  //               margin: "auto",
+  //               backgroundColor: "#9DCFFF",
+  //             }
+  //           : {
+  //               borderBottom: "solid #9DCFFF 1px",
+  //               width: "90%",
+  //               margin: "auto",
+  //               backgroundColor: "white",
+  //             }
+  //       }
+  //     >
+  //       <ListItemButton onClick={Clicklistitem}>
+  //         <ListItemText primary={index + 1 + `번문제`} />
+  //       </ListItemButton>
+  //     </ListItem>
+  //   );
+  // }
   const dispatch = useDispatch();
   const questionIndex = useSelector((state) => state.QuestionInfo.questionIndex);
   useEffect(() => {
@@ -52,56 +52,42 @@ export default function SideBar(props) {
   }, []);
   console.log("props:", props);
   return (
-    <Box
-      sx={{
-        mt: 3,
-        mr: 2,
-        ml: 1,
-        width: "70%",
-        height: "100%",
-        maxWidth: 360,
-        bgcolor: "background.paper",
-        borderColor: "#0099FF",
-        borderStyle: "solid",
-        // overflow: "scroll",
-      }}
-    >
-      <List sx={{ width: "100%", maxWidth: 360 }}>
-        {props.questionList.map((data, index) => {
-          console.log(data, index);
-          const current = index === questionIndex;
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      {props.questionList.map((data, index) => {
+        console.log(data, index);
+        const current = index === questionIndex;
 
-          const Clicklistitem = () => {
-            dispatch(SET_QUESTION_INDEX(index));
-          };
-          return (
-            <ListItem
-              key={data}
-              // component="div"
-              // disablePadding
-              style={
-                current
-                  ? {
-                      borderBottom: "solid #9DCFFF 1px",
-                      width: "90%",
-                      margin: "auto",
-                      backgroundColor: "#9DCFFF",
-                    }
-                  : {
-                      borderBottom: "solid #9DCFFF 1px",
-                      width: "90%",
-                      margin: "auto",
-                      backgroundColor: "white",
-                    }
-              }
-            >
-              <ListItemButton onClick={Clicklistitem}>
-                <ListItemText primary={index + 1 + `번문제`} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
-    </Box>
+        const Clicklistitem = () => {
+          console.log("click ", index);
+          dispatch(SET_QUESTION_INDEX(index));
+        };
+        return (
+          <ListItem
+            key={data}
+            // component="div"
+            // disablePadding
+            style={
+              current
+                ? {
+                    borderBottom: "solid #9DCFFF 1px",
+                    width: "90%",
+                    margin: "auto",
+                    backgroundColor: "#9DCFFF",
+                  }
+                : {
+                    borderBottom: "solid #9DCFFF 1px",
+                    width: "90%",
+                    margin: "auto",
+                    backgroundColor: "white",
+                  }
+            }
+          >
+            <ListItemButton onClick={Clicklistitem}>
+              <ListItemText primary={index + 1 + `번문제`} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
   );
 }
